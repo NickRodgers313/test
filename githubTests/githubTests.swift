5a4002e7937832afbd3e5d0d55de777bebdb99a8
Nick Rodgers <nicholasrodgers342@gmail.com>
2024-12-11 16:00:28 -0500

    Initial Commit

---
+++ githubTests/githubTests.swift
@@ -0,0 +1,17 @@
+//
+//  githubTests.swift
+//  githubTests
+//
+//  Created by nick rodgers on 12/11/24.
+//
+
+import Testing
+@testable import github
+
+struct githubTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
